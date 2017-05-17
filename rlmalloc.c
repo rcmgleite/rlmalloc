83174d0abad47ec99b1603f369d64865236a7b5e
Rafael Leite <rafael.leite@controlid.com.br>
2017-05-17 00:34:16 -0300

    minor refactors and fixing stupid ptrptr error

--- rlmalloc.c
+++ rlmalloc.c
@@ -8,24 +8,22 @@
 /**
  *  Global variable that holds a pointer to the end of the heap
  */
-static void* heap_ptr=NULL;
-
-typedef struct block_metadata* block_metadata_ptr;
+static void* heap_base_ptr=NULL;
 
 /**
  *  Block metadata
  *    Is a double-linked list
  */
-struct block_metadata {
+typedef struct block_metadata {
   size_t size;
-  block_metadata_ptr next;
-  block_metadata_ptr prev;
+  struct block_metadata* next;
+  struct block_metadata* prev;
   bool is_free;
   void* validation_ptr;
   char block_metadata_end[0]; // http://gcc.gnu.org/onlinedocs/gcc/Zero-Length.html
-};
+} Block_metadata;
 
-#define BLOCK_METADATA_SIZE sizeof(struct block_metadata)
+#define BLOCK_METADATA_SIZE sizeof(Block_metadata)
 
 /*
  *  All pointers returned by malloc will be aligned for long type.
@@ -46,20 +44,20 @@ static inline size_t align_long(size_t size) {
 /*
  *  a suitable block is one that is free and has at least the size we are asking for
  */
-static inline bool is_suitable_block(block_metadata_ptr block, size_t size) {
+static inline bool is_suitable_block(Block_metadata* block, size_t size) {
   return block->is_free && block->size >= size;
 }
 
 /**
  *  Returns a block if it finds a suitable one or NULL otherwise
  *
- *  @param block_metadata_ptr last is used just to make it easy to extend
+ *  @param Block_metadata* last is used just to make it easy to extend
  *    the heap in case no suitable block is found
  */
-block_metadata_ptr get_free_block(block_metadata_ptr last, size_t size) {
-  block_metadata_ptr block_runner = (block_metadata_ptr)heap_ptr;
+Block_metadata* get_free_block(Block_metadata** last, size_t size) {
+  Block_metadata* block_runner = (Block_metadata*)heap_base_ptr;
   while (block_runner && !(is_suitable_block(block_runner, size))) {
-    last = block_runner;
+    *last = block_runner;
     block_runner = block_runner->next;
   }
 
@@ -69,12 +67,12 @@ block_metadata_ptr get_free_block(block_metadata_ptr last, size_t size) {
 /*
  *  Extends the heap using srbk like system calls
  * 
- *  @param block_metadata_ptr last: pointer to the last block allocated before a call to extend_heap
+ *  @param Block_metadata* last: pointer to the last block allocated before a call to extend_heap
  *  @param size_t size: size of the requested new block
  */
-block_metadata_ptr extend_heap(block_metadata_ptr last, size_t size) {
-  block_metadata_ptr block;
-  block = (block_metadata_ptr)sbrk(0);
+Block_metadata* extend_heap(Block_metadata* last, size_t size) {
+  Block_metadata* block;
+  block = (Block_metadata*)sbrk(0);
 
   if (sbrk(BLOCK_METADATA_SIZE + size) == (void*)-1) { // sbrk failed
     return NULL;
@@ -96,11 +94,11 @@ block_metadata_ptr extend_heap(block_metadata_ptr last, size_t size) {
 /**
  *  Splits @block to have the @size size and creates a new block with the remaining space
  *
- *  @parm block_metadata_ptr block: block to be splitted
+ *  @parm Block_metadata* block: block to be splitted
  *  @size_t size new block size
  */
-void split_block(block_metadata_ptr block, size_t size) {
-  block_metadata_ptr new_block = (block_metadata_ptr) block->block_metadata_end + size; // important to have the block_metadata_end as char[0] for the pointer arithmetic
+void split_block(Block_metadata* block, size_t size) {
+  Block_metadata* new_block = (Block_metadata*) block->block_metadata_end + size; // important to have the block_metadata_end as char[0] for the pointer arithmetic
   new_block->size = block->size - size - BLOCK_METADATA_SIZE;
   new_block->next = block->next;
   new_block->prev = block;
@@ -119,18 +117,18 @@ void split_block(block_metadata_ptr block, size_t size) {
  */
 void* rlmalloc(size_t size) {
   size_t aligned_size = align_long(size);
-  block_metadata_ptr block_to_return = NULL;
+  Block_metadata* block_to_return = NULL;
 
-  if (!heap_ptr) { // first allocation
+  if (!heap_base_ptr) { // first allocation
     block_to_return = extend_heap(NULL, aligned_size);
     if (!block_to_return) {
       return NULL;
     }
     
-    heap_ptr = block_to_return;
+    heap_base_ptr = block_to_return;
   } else {
-    block_metadata_ptr last_heap_ptr = (block_metadata_ptr)heap_ptr;
-    block_to_return = get_free_block(last_heap_ptr, aligned_size);
+    Block_metadata* last_heap_ptr = (Block_metadata*)heap_base_ptr;
+    block_to_return = get_free_block(&last_heap_ptr, aligned_size);
     if (block_to_return) { // There is a free block already got from OS
       if (block_to_return->size - aligned_size >= BLOCK_METADATA_SIZE + sizeof(long)) {
         split_block(block_to_return, aligned_size);
@@ -138,7 +136,7 @@ void* rlmalloc(size_t size) {
       
       block_to_return->is_free = false;
     } else { // No free blocks, we must ask more memory for the OS
-      block_to_return = extend_heap(heap_ptr, aligned_size);
+      block_to_return = extend_heap(last_heap_ptr, aligned_size);
       if (!block_to_return) {
         return NULL;
       }
@@ -152,9 +150,9 @@ void* rlmalloc(size_t size) {
  *  If a block is next to other empty blocks, merges them into one
  *    This is a way to minimize memory fragmentatio
  *
- *  @param block_metadata_ptr block
+ *  @param Block_metadata* block
  */
-block_metadata_ptr merge_block_with_next(block_metadata_ptr block) {
+Block_metadata* merge_block_with_next(Block_metadata* block) {
   if (block->next && block->next->is_free) {
     block->size += BLOCK_METADATA_SIZE + block->next->size;
     block->next = block->next->next;
@@ -172,14 +170,14 @@ block_metadata_ptr merge_block_with_next(block_metadata_ptr block) {
  *
  *  @param void* ptr
  */
-block_metadata_ptr get_block_metadata_from_ptr(void* ptr) {
+Block_metadata* get_block_metadata_from_ptr(void* ptr) {
   char* aux = (char*)ptr; // needed for the pointer arithmetic on the return statement
-  return (block_metadata_ptr)(aux - BLOCK_METADATA_SIZE);
+  return (Block_metadata*)(aux - BLOCK_METADATA_SIZE);
 }
 
 bool is_valid_ptr(void* ptr) {
-  if (heap_ptr) {
-    if (ptr > heap_ptr && ptr < sbrk(0)) { // heap range check
+  if (heap_base_ptr) {
+    if (ptr > heap_base_ptr && ptr < sbrk(0)) { // heap range check
       return (ptr == get_block_metadata_from_ptr(ptr)->validation_ptr); // using the validation_ptr we are able to validate @ptr
     }
   }
@@ -196,11 +194,11 @@ bool is_valid_ptr(void* ptr) {
  */
 void rlfree(void* ptr) {
   if (!is_valid_ptr(ptr)) {
-    printf("Invalid ptr passed to 'free'");
+    printf("\nInvalid ptr passed to 'free'\n");
     return;
   }
 
-  block_metadata_ptr block = get_block_metadata_from_ptr(ptr);
+  Block_metadata* block = get_block_metadata_from_ptr(ptr);
   block->is_free = true;
 
   if (block->prev && block->prev->is_free) {
@@ -213,7 +211,7 @@ void rlfree(void* ptr) {
     if (block->prev) { // There are more blocks to the left
       block->prev->next = NULL;
     } else { // all heap is now back to the OS
-      heap_ptr = NULL;
+      heap_base_ptr = NULL;
     }
 
     brk(block);
